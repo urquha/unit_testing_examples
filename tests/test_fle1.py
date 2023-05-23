7e3bc653b8c48cc4a3cae8503a50fad75f8728d9
urquha <andy.urquhart@hotmail.co.uk>
2023-05-23 12:52:31 +0100

    Added no mock test

--- tests/test_fle1.py
+++ tests/test_fle1.py
@@ -1,14 +1,25 @@
 from unittest.mock import patch, Mock
 from  data.module_d import function_d
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
 
 @patch('data.module_b.function_a')
-def test_function_e(mock_function_a):
+def test_function_d_with_mock(mock_function_a):
     # Replace function_a with a mock
     mock_function_a.return_value = "Mocked A"
 
-    # Call function_e
+    # Call function_d
     result = function_d()
 
     # Assert something about the result
     assert result == "Mocked A", "Function E did not return the expected result"
+
+def test_function_d_no_mock():
+
+    # Call function_d
+    result = function_d()
+
+    # Assert something about the result
+    assert_frame_equal(result, pd.DataFrame())
 
